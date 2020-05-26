d75598e860a4244b4597365959180a938e276fdd
Игнатов Илья <64230361+e100nets@users.noreply.github.com>
2020-05-26 19:33:12 +0300

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -17,7 +17,7 @@ public class GUI extends JFrame {
     private JPanel jPanel = new JPanel();
     private JLabel countElements = new JLabel("Количество элементов списка: ");
     private JLabel maxElement = new JLabel("Максимальное число: ");
-    private JLabel countMax = new JLabel("Количество максимальных элементов");
+    private JLabel countMax = new JLabel("Количество максимальных элементов: ");
     private JButton loadList = new JButton("Загрузить из файла");
     private JButton randomList = new JButton("Рандомный список");
     private JButton findMax = new JButton("Выполнить программу");
@@ -55,8 +55,8 @@ public class GUI extends JFrame {
                     linkList.clear();
                     if (fileChooser.showOpenDialog(jPanel) == JFileChooser.APPROVE_OPTION) {
                         int[] array = ArrayUtils.readIntArrayFromFile(fileChooser.getSelectedFile().getPath());
+                        linkList.listFromArray(array);
                         JTableUtils.writeArrayToJTable(jTable, array);
-                        linkList.listFromArray(JTableUtils.readIntArrayFromJTable(jTable));
                     }
                 } catch (Exception e) {
                     SwingUtils.showErrorMessageBox(e);
@@ -72,23 +72,35 @@ public class GUI extends JFrame {
             }
         });
 
-        Container container = this.getContentPane();
-        container.setLayout(new FlowLayout(FlowLayout.CENTER));
-        container.add(countElements);
-        container.add(size);
-        container.add(jTable);
-        container.add(maxElement);
-        container.add(max);
-        container.add(countMax);
-        container.add(count);
-        container.add(randomList);
-        container.add(loadList);
-        container.add(findMax);
+        jPanel.setLayout(null);
+        countElements.setBounds(5,5, 250, 20);
+        size.setBounds(260, 5, 50, 20);
+        jTable.setBounds(5, 30, 200, 20);
+        loadList.setBounds(5, 55, 200, 30);
+        randomList.setBounds(5,90, 200, 30);
+        findMax.setBounds(5,125, 200, 30);
+        maxElement.setBounds(5, 160, 180, 30);
+        max.setBounds(190, 160, 50, 20);
+        countMax.setBounds(5, 185, 280, 20);
+        count.setBounds(290, 185, 50, 20);
+
+        jPanel.add(countElements);
+        jPanel.add(size);
+        jPanel.add(jTable);
+        jPanel.add(loadList);
+        jPanel.add(randomList);
+        jPanel.add(findMax);
+        jPanel.add(maxElement);
+        jPanel.add(max);
+        jPanel.add(countMax);
+        jPanel.add(count);
+
+        getContentPane().add(jPanel);
     }
 
     public static void main (String [] args) {
         GUI app = new GUI();
-        app.setBounds(100, 100, 1000, 200);
+        app.setBounds(100, 100, 600, 300);
         app.setVisible(true);
     }
 }
